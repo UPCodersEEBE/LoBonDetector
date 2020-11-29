4e98106f1a086daf8d0d7ae06f714f65cf20d3f3
totimorpa <totimorpa@gmail.com>
2020-11-29 03:12:42 +0100

    GUI implementation

--- app.py
+++ app.py
@@ -5,14 +5,36 @@ Created on Sun Nov 29 00:53:01 2020
 
 @author: clara
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import main
 
 app = Flask(__name__)
 
+# image_path = "resources/t485. 09.40.33.jpg"
 
-@app.route("/")
+# final_state = main.complete_function(image_path)
+
+# final_state = [[1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+# [1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+# [1, 1, 1, 1, 'X', 'X', 'X', 1, 1, 1, 1, 1, 1, 1],
+# [1, 1, 1, 0, 1, 'X', 'X', 'X', 'X', 1, 1, 1, 1, 1, 1],
+# [1, 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 1, 1, 1]]
+
+
+
+@app.route("/form")
+def form():
+    files=os.listdir("resources")
+    return render_template('form.html', files=files)
+
+@app.route("/", methods = ["GET", "POST"])
 def template_test():
-    return render_template('sample2.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+    select = request.form.get('comp_select')
+    print(select)
+
+    final_state = main.complete_function("resources/" + select)
+    return render_template('sample.html', final_state=final_state)
 
 
 app.run(debug=True, use_reloader=False)
--- main.py
+++ main.py
@@ -4,35 +4,28 @@ import Prediction
 import os
 import time
  
-estat_estanteries = [[],[],[],[],[]]
+def complete_function(PhotoToAnalize):
+    estat_estanteries = [[],[],[],[],[]]
+    PhotoSplitter.get_product_slots(PhotoToAnalize)
 
-PhotoToAnalize = "resources/t485. 09.44.27.jpg" # escollim la foto a analitzar
+    c = []
+    for file in os.listdir("product_slots"):
+        c.append(file)
+    c.sort()
 
+    m = google_annotate.analyze_image(PhotoToAnalize)
+    x = 0
+    f = 0
+    for i in m:
+        for j in i:
+            if j == 1:
+                estat_estanteries[f].append("X")
+            else:
+                estat_estanteries[f].append(Prediction.prediccio("product_slots/%s" % c[x]))
+            x += 1
+        f +=1
+    for file in os.listdir("product_slots"):
+        os.remove("product_slots/%s" % file)
+    return(estat_estanteries)
 
-PhotoSplitter.get_product_slots(PhotoToAnalize)
-
-c = []
-for file in os.listdir("product_slots"):
-    c.append(file)
-c.sort()
-
-m = google_annotate.analyze_image(PhotoToAnalize)
-
-
-x = 0
-f = 0
-for i in m:
-    for j in i:
-        if j == 1:
-            estat_estanteries[f].append("X")
-        else:
-            estat_estanteries[f].append(Prediction.prediccio("product_slots/%s" % c[x]))
-        x += 1
-    f +=1
-
-
-for i in estat_estanteries:
-    print(i)
-
-for file in os.listdir("product_slots"):
-    os.remove("product_slots/%s" % file)
+print(complete_function("resources/t485. 09.40.33.jpg"))

16e7fe0625984bfc376926170ad7ed7555d770a4
totimorpa <totimorpa@gmail.com>
2020-11-29 09:57:24 +0100

    main update

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import Prediction
 import os
 import time
  
-<<<<<<< HEAD
-=======
 def compta_buits(final_state):
     buits=0
     for fila in final_state:
@@ -14,7 +12,6 @@ def compta_buits(final_state):
                 buits+=1
     return buits
     
->>>>>>> 04eb6c482248460689a3589d1a3d70c99b9ff2b7
 def complete_function(PhotoToAnalize):
     estat_estanteries = [[],[],[],[],[]]
     PhotoSplitter.get_product_slots(PhotoToAnalize)
@@ -38,11 +35,3 @@ def complete_function(PhotoToAnalize):
     for file in os.listdir("product_slots"):
         os.remove("product_slots/%s" % file)
     return(estat_estanteries)
-
-def compta_buits(final_state):
-    buits=0
-    for fila in final_state:
-        for item in final_state:
-            if item==0:
-                buits+=1
-    return buits
